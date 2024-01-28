215015eea4ccab7be5b6b28c821c0d41483fa209
Abhishek <akumarfun@gmail.com>
2024-01-28 17:53:44 +0900

    Requirement 1 to 6 completed

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,15 +3,24 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 import com.google.common.base.Utf8;
+import db.DataBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import model.User;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -21,27 +30,115 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (InputStream in = connection.getInputStream();
+             OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             BufferedReader bufferedReader = new BufferedReader
-                    (new InputStreamReader(in,"UTF-8"));
+                    (new InputStreamReader(in, "UTF-8"));
             String httpDataLine = bufferedReader.readLine();
-            String tokens[] = httpDataLine.split(" ");
-            log.info("url:" + httpDataLine);
-            if(httpDataLine == null)
+            if (httpDataLine == null)
                 return;
-
-            while(!httpDataLine.equals(""))
-            {
+            String[] tokens = httpDataLine.split(" ");
+            String httpMethod = tokens[0];
+            String url = tokens[1];
+            log.info("url:" + url);
+            log.info("firstline:" + httpDataLine);
+            String urltype = analyseurl(url);
+            int contentLength = 0;
+            boolean logined = false;
+            while (!httpDataLine.equals("")) {
                 httpDataLine = bufferedReader.readLine();
+                if (httpDataLine.contains("Content-Length")) {
+                    log.info("content-lengththing:" +
+                            Arrays.toString(httpDataLine.split(" ")));
+                    contentLength = Integer.parseInt(httpDataLine.split(" ")[1]);
+                }
+                if (httpDataLine.contains("Cookie")) {
+                    Map<String,String> cookieMap =  HttpRequestUtils.
+                            parseCookies(httpDataLine.split(" ")[1]);
+                    logined = Boolean.parseBoolean(cookieMap.get("logined"));
+                    log.info("logined:" +logined);
+                }
                 log.info(httpDataLine);
             }
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body= {};
-            body = Files.readAllBytes(new File("./webapp" +
-                        tokens[1]).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            byte[] body = {};
+            String params;
+            if (urltype.equals("filerequest")) {
+                body = Files.readAllBytes(new File("./webapp" +
+                        url).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            } else if (urltype.equals("createuser")) {
+                User webUser = null;
+                params = "";
+                if (httpMethod.equals("GET")) {
+                    int index = url.indexOf("?");
+//                    String requestPath = url.substring(0, index);
+                    params = url.substring(index + 1);
+
+                } else if (httpMethod.equals("POST")) {
+                    params = IOUtils.readData(bufferedReader, contentLength);
+
+                }
+                Map<String, String> paramsMap = HttpRequestUtils.parseQueryString(params);
+                webUser = new User(paramsMap.get("userId"), paramsMap.get("password"),
+                        paramsMap.get("name"), paramsMap.get("email"));
+                DataBase.addUser(webUser);
+                log.info("new user created:" + webUser);
+                body = "new user created".getBytes();
+                response302Header(dos, "/index.html",false,null);
+            } else if (urltype.equals("loginuser")) {
+                params = IOUtils.readData(bufferedReader, contentLength);
+                Map<String, String> paramsMap = HttpRequestUtils.parseQueryString(params);
+                User dbUser = DataBase.findUserById(paramsMap.get("userId"));
+                if(dbUser!= null && (dbUser.getUserId().equals(paramsMap.get("userId"))) &&
+                        (dbUser.getPassword().equals(paramsMap.get("password"))))
+                {
+                    log.info("user credentials matched with saved user in db");
+                    response302Header(dos, "/index.html",true,
+                            "logined=true");
+                }
+                else{
+                    response302Header(dos, "/user/login_failed.html",true,
+                            "logined=false");
+                }
+            }
+            else if(urltype.equals("listuser"))
+            {
+                  if(logined)
+                  {
+                      Collection<User> userList = DataBase.findAll();
+                      StringBuilder stringBuilder = new StringBuilder();
+                      stringBuilder.append("<table>");
+                      stringBuilder.append("<tr>");
+                      stringBuilder.append("<th>Userid</th>");
+                      stringBuilder.append("<th>Name</th>");
+                      stringBuilder.append("<th>Email</th>");
+                      for(User u: userList)
+                      {
+                          stringBuilder.append("<tr>");
+                          stringBuilder.append("<td>"+u.getUserId()+"</td>");
+                          stringBuilder.append("<td>"+u.getName()+"</td>");
+                          stringBuilder.append("<td>"+u.getEmail()+"</td>");
+                          stringBuilder.append("<tr>");
+                      }
+                      stringBuilder.append("<table>");
+                      body = stringBuilder.toString().getBytes();
+                      response200Header(dos, body.length);
+                      responseBody(dos, body);
+                  }
+                  else {
+                      response302Header(dos, "/user/login.html",false,
+                              null);
+                  }
+            }
+            else {
+                body = "Hello World".getBytes();
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
@@ -58,6 +155,21 @@ public class RequestHandler extends Thread {
         }
     }
 
+    private void response302Header(DataOutputStream dos, String url,
+                                   boolean setcookie, String cookievalue) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            if(setcookie)
+            {
+                dos.writeBytes("Set-Cookie: "+cookievalue+"\r\n");
+            }
+            dos.writeBytes("Location: " + url + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
             dos.write(body, 0, body.length);
@@ -65,5 +177,19 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String analyseurl(String url)
+    {
+        if(url.contains(".html") || url.contains(".ico"))
+            return "filerequest";
+        if(url.startsWith("/user/create"))
+            return "createuser";
+        if(url.startsWith("/user/login"))
+            return "loginuser";
+        if(url.startsWith("/user/list"))
+            return "listuser";
+
+        return "helloworld";
     }
 }
